64141680860300f448118451346eda987dc6104a
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2024-10-12 14:57:15 -0500

    added hosts

--- backend_settings/settings.py
+++ backend_settings/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-#^d7u%xz=(7i68t-3$c0n78nq*#@64^tict7+k=gmjz&salh7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
